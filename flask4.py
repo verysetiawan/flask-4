c3969b0f3487c18da0853a31f8321c706cc87af7
verysetiawan <kangphery@gmail.com>
2020-05-18 17:00:53 +0700

    menambahkan redirect halaman

--- flask4.py
+++ flask4.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "IniSecretKeyKu2020"
@@ -17,6 +17,11 @@ def about():
 @app.route ("/contact")
 def contact():
     return render_template("contact.html")
+
+#menambahkan halaman redirect
+@app.route ("/redirect")
+def redir():
+    return redirect(url_for("about"))
 
 
 if __name__ == "__main__":
