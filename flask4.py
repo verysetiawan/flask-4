ca7e136204e44c0416abda969121aed124f58cde
verysetiawan <kangphery@gmail.com>
2020-05-18 16:33:33 +0700

    Commit Pertama

--- flask4.py
+++ flask4.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "IniSecretKeyKu2020"
+
 
 @app.route("/")
-def hello():
-    return "hello world!!!"
+def index():
+    return render_template ("index.html")
 
 if __name__ == "__main__":
-    app.run (host='0.0.0.0', debug=True)
+    app.run (host='0.0.0.0', debug=True, port=5001)
