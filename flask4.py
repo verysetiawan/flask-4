a97f87ebf720083404b6669731303863512845c1
verysetiawan <kangphery@gmail.com>
2020-05-18 16:49:53 +0700

    url for

--- flask4.py
+++ flask4.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "IniSecretKeyKu2020"
